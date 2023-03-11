d0bdc8408ba9b0a8a53e5e67688ca4e2d8661471
Aly Mamdouh <Aly.Mamdouh@InteractTS.com>
2023-03-11 14:48:27 +0200

    register the controller

---
+++ BuberDinnerApp.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,21 @@
+﻿using BuberDinnerApp.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinnerApp.Api.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpPost("register")]
+        public IActionResult Register(RegisterRequest registerRequest)
+        {
+            return Ok(registerRequest);
+        }
+        [HttpPost("login")]
+        public IActionResult Login(LoginRequest loginRequest)
+        {
+            return Ok(loginRequest);
+        }
+    }
+}
